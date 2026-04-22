f86006a8e50c9107b42fefef600e0b9d37d4e4fd
CLARA GABRIELLE BONONI <clara.bononi@portalsesisp.org.br>
2026-04-22 14:47:32 -0300

    realizando a autenticação e proteção de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import ClienteController from "./controller/ClienteController.js";
 import PedidoController from "./controller/PedidoController.js";
 import ProdutoController from "./controller/ProdutoController.js";
 import { Auth } from "./middlewares/Auth.js";
+import type { Request, Response, NextFunction } from "express";
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -10,29 +12,26 @@ router.get('/', (req, res) => {
         timestamp: `${new Date().toLocaleString('pt-br')}`});
 });
 
-
 router.post('/api/login', Auth.validacaoUsuario);
 
-router.use('/api', Auth.verifyToken);
-
-// CLIENTE
-router.get('/api/cliente', ClienteController.todos);
-router.post('/api/cliente', ClienteController.novo);
-router.get('/api/cliente/:idCliente', ClienteController.id);
-
-// PEDIDO
-router.get('/api/pedido', PedidoController.todos);
-router.get('/api/pedido/:idPedido', PedidoController.id);
-router.post('/api/pedido', PedidoController.novo);
-
-// PRODUTO
-router.get('/api/produto', ProdutoController.todos);
-router.post('/api/produto', ProdutoController.novo);
-router.get('/api/produto/:idProduto', ProdutoController.id);
-
-
-router.get('/api/protegido', (req, res) => {
-    res.status(200).send('OK protegido');
+router.use(Auth.verifyToken);
+router.get('/api/protegido', (req: Request, res: Response) => {
+    res.send('Rota protegida');
 });
+
+router.get(`/api/cliente`, ClienteController.todos);
+router.post(`/api/cliente`, ClienteController.novo);
+router.get(`/api/cliente/:idCliente`, ClienteController.id);
+
+router.get(`/api/pedido`, PedidoController.todos);
+router.get(`/api/pedido/:idPedido`, PedidoController.id);
+router.post(`/api/pedido`, PedidoController.novo);
+
+router.get(`/api/produto`, ProdutoController.todos);
+router.post(`/api/produto`, ProdutoController.novo);
+router.get(`/api/produto/:idProduto`, ProdutoController.id);
+
+
+
 
 export { router };
