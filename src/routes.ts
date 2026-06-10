662edb044c8307a244dea269cdeee8368070290d
SOFIA MARTINS DE SOUZA <sofia.souza8@portalsesisp.org.br>
2026-06-10 11:19:47 -0300

    fazendo sprint 11

--- src/routes.ts
+++ src/routes.ts
@@ -24,6 +24,7 @@ router.post(`/api/cliente`, ClienteController.novo);
 router.get(`/api/cliente/:idCliente`, ClienteController.id);
 
 router.get(`/api/pedido`, PedidoController.todos);
+router.get("/api/pedido/detalhado", PedidoController.detalhados);
 router.get(`/api/pedido/:idPedido`, PedidoController.id);
 router.post(`/api/pedido`, PedidoController.novo);
 
