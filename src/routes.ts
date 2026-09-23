e888cc38e31d101b7e34f2320ff0e483fd17a7f8
LAVÍNIA MARIA SILVA CAMPOS <lavinia.campos806@portalsesisp.org.br>
2026-09-23 09:08:25 -0300

    feat: descrição da alteração

--- src/routes.ts
+++ src/routes.ts
@@ -21,7 +21,10 @@ router.get('/api/protegido', (req: Request, res: Response) => {
 
 router.get(`/api/cliente`, ClienteController.todos);
 router.post(`/api/cliente`, ClienteController.novo);
-router.get(`/api/cliente/:idCliente`, ClienteController.id);
+router.get(`/api/cliente/:id`, ClienteController.cliente);
+router.put(`/api/cliente/:id`, ClienteController.atualizar);
+router.patch(`/api/cliente/:id`, ClienteController.atualizar);
+router.delete(`/api/cliente/:id`, ClienteController.remover);
 
 router.get(`/api/pedido`, PedidoController.todos);
 router.get(`/api/pedido/baratos`, PedidoController.listarPedidoBaixo);
@@ -29,10 +32,16 @@ router.get(`/api/pedido/completos`, PedidoController.listarPedidoCompleto);
 router.get(`/api/pedido/detalhado`, PedidoController.detalhados);
 router.get(`/api/pedido/:idPedido`, PedidoController.id);
 router.post(`/api/pedido`, PedidoController.novo);
+router.put(`/api/pedido/:idPedido`, PedidoController.atualizar);
+router.patch(`/api/pedido/:idPedido`, PedidoController.atualizar);
+router.delete(`/api/pedido/:idPedido`, PedidoController.remover);
 
 router.get(`/api/produto`, ProdutoController.todos);
 router.post(`/api/produto`, ProdutoController.novo);
 router.get(`/api/produto/:idProduto`, ProdutoController.id);
+router.put(`/api/produto/:idProduto`, ProdutoController.atualizar);
+router.patch(`/api/produto/:idProduto`, ProdutoController.atualizar);
+router.delete(`/api/produto/:idProduto`, ProdutoController.remover);
 
 
 
