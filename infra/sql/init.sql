72a14fc9f8a77712be6bf9ce4de7f6096de1358e
Heloísa Marcovecchio <hrozzette@gmail.com>
2026-09-25 16:02:06 -0300

    Sprint 15

--- infra/sql/init.sql
+++ infra/sql/init.sql
@@ -21,9 +21,17 @@ CREATE TABLE Pedido (
     id_produto INT NOT NULL,
     data_pedido DATE NOT NULL,
     valor_total DECIMAL (10,2) NOT NULL,
-    status VARCHAR (15) NOT NULL,
+    status_pedido VARCHAR (15) NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
     FOREIGN KEY (id_produto) REFERENCES Produto (id_produto)
+);
+
+CREATE TABLE Usuario (
+    id_usuario INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    email VARCHAR(120) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
 );
 
     FOREIGN KEY (id_cliente)
@@ -103,8 +111,8 @@ VALUES
 ('Bruno Costa', 'bruno.costa@email.com', 'Rua das Acácias, 400 - Vila Atlântica', '13991100998', '01234567890');
 
 
-INSERT INTO Usuarios
-(nome, email, senha)
+INSERT INTO Pedido
+(id_cliente, id_produto, data_pedido, valor_total, status_pedido)
 VALUES
 (1, 1, '2026-02-20', 18.90, 'entregue'),
 (2, 3, '2026-02-21', 24.90, 'preparando'),
@@ -117,46 +125,275 @@ VALUES
 (9, 1, '2026-02-25', 18.90, 'à caminho'),
 (10, 3, '2026-02-25', 24.90, 'pedido aceito');
 
-UPDATE cliente
-SET email = 'ana.souza@email.com'
-WHERE id_cliente = 1;
-
-UPDATE cliente
-SET email = 'carlos.mendes@email.com'
-WHERE id_cliente = 2;
-
-UPDATE cliente
-SET email = 'juliana.lima@email.com'
-WHERE id_cliente = 3;
-
-UPDATE cliente
-SET email = 'marcos.oliveira@email.com'
-WHERE id_cliente = 4;
-
-UPDATE cliente
-SET email = 'fernanda.rocha@email.com'
-WHERE id_cliente = 5;
-
-UPDATE cliente
-SET email = 'ricardo.alves@email.com'
-WHERE id_cliente = 6;
-
-UPDATE cliente
-SET email = 'patricia.gomes@email.com'
-WHERE id_cliente = 7;
-
-UPDATE cliente
-SET email = 'lucas.ferreira@email.com'
-WHERE id_cliente = 8;
-
-UPDATE cliente 
-SET email = 'camila.santos@email.com'
-WHERE id_cliente = 9;
-
-UPDATE cliente
-SET email = 'bruno.costa@email.com'
-WHERE id_cliente = 10;
+INSERT INTO Produto (nome_produto, preco, disponibilidade, cod_produto)
+VALUES
+('X-Burger', 18.90, 'disponível', 11),
+('X-Salada', 20.90, 'disponível', 12),
+('X-Bacon', 24.90, 'disponível', 13),
+('Batata Frita Média', 15.00, 'disponível', 14),
+('BATATA FRITA GRANDE', 67.00, 'disponível', 15),
+('Refrigerante Lata', 6.00, 'disponível', 16),
+('Suco Natural', 8.50, 'disponível', 17),
+('Milkshake Chocolate', 16.00, 'indisponível', 18),
+('Hot Dog Especial', 17.50, 'disponível', 20),
+('Combo Família', 79.90, 'disponível', 22),
+('X-TUDO ESPECIAL', 25.90, 'disponível', 24),
+('X EGG SALADA', 22.90, 'Disponível', 25),
+('X BIG DA CASA', 27.99, 'Disponível', 26);
 
 INSERT INTO usuario (nome, email, senha, role)
 VALUES ('Admin', 'admin@email.com', '1234', 'admin');
 
+CREATE OR REPLACE VIEW public.vw_pedidos_detalhados
+ AS
+ SELECT p.id_pedido,
+    c.nome,
+    c.telefone,
+    pr.nome_produto,
+    pr.preco,
+    p.data_pedido,
+    p.valor_total,
+    p.status_pedido
+   FROM pedido p
+     JOIN cliente c ON p.id_cliente = c.id_cliente
+     JOIN produto pr ON p.id_produto = pr.id_produto;
+
+ALTER TABLE public.vw_pedidos_detalhados
+    OWNER TO postgres;
+
+ALTER TABLE Cliente
+ADD CONSTRAINT unique_email UNIQUE (email);
+
+ALTER TABLE Produto
+ADD CONSTRAINT check_preco
+CHECK (preco >= 0);
+
+ALTER TABLE Pedido
+ADD CONSTRAINT check_status
+CHECK (status IN ('entregue', 'preparando', 'à caminho', 'pedido aceito'));
+
+CREATE OR REPLACE FUNCTION padronizar_status()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.status_pedido := INITCAP(LOWER(NEW.status_pedido));
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_padronizar_status ON Pedido;
+
+CREATE TRIGGER trigger_padronizar_status
+BEFORE INSERT OR UPDATE ON Pedido
+FOR EACH ROW
+EXECUTE FUNCTION padronizar_status();
+
+UPDATE Pedido
+SET status_pedido = INITCAP(LOWER(status_pedido));
+
+ALTER TABLE Pedido
+ADD CONSTRAINT check_status
+CHECK (status_pedido IN (
+    'Pedido Aceito',
+    'Concluído',
+    'À Caminho',
+    'Preparando',
+    'Entregue',
+    'Pendente'
+));
+
+CREATE OR REPLACE FUNCTION fn_primeira_letra_maiuscula()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.nome_produto :=
+        UPPER(LEFT(TRIM(NEW.nome_produto), 1)) ||
+        SUBSTRING(TRIM(NEW.nome_produto) FROM 2);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_primeira_letra_maiuscula
+BEFORE INSERT OR UPDATE ON Produto
+FOR EACH ROW
+EXECUTE FUNCTION fn_primeira_letra_maiuscula();
+
+UPDATE Produto
+SET nome_produto =
+    UPPER(LEFT(TRIM(nome_produto), 1)) ||
+    SUBSTRING(TRIM(nome_produto) FROM 2);
+
+    UPDATE Produto
+SET nome_produto =
+    UPPER(LEFT(LOWER(TRIM(nome_produto)), 1)) ||
+    SUBSTRING(LOWER(TRIM(nome_produto)) FROM 2);
+
+    UPDATE Cliente
+SET nome =
+    UPPER(LEFT(LOWER(TRIM(nome)), 1)) ||
+    SUBSTRING(LOWER(TRIM(nome)) FROM 2);
+
+    CREATE OR REPLACE FUNCTION fn_primeira_letra_nome_cliente()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.nome :=
+        UPPER(LEFT(LOWER(TRIM(NEW.nome)), 1)) ||
+        SUBSTRING(LOWER(TRIM(NEW.nome)) FROM 2);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_primeira_letra_nome_cliente
+BEFORE INSERT OR UPDATE ON Cliente
+FOR EACH ROW
+EXECUTE FUNCTION fn_primeira_letra_nome_cliente();
+
+UPDATE Cliente
+SET endereco = INITCAP(TRIM(endereco));
+
+CREATE OR REPLACE FUNCTION fn_primeira_letra_endereco_cliente()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.endereco := INITCAP(TRIM(NEW.endereco));
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_primeira_letra_endereco_cliente
+BEFORE INSERT OR UPDATE ON Cliente
+FOR EACH ROW
+EXECUTE FUNCTION fn_primeira_letra_endereco_cliente();
+
+UPDATE Produto
+SET disponibilidade =
+    UPPER(LEFT(LOWER(TRIM(disponibilidade)), 1)) ||
+    SUBSTRING(LOWER(TRIM(disponibilidade)) FROM 2);
+
+    CREATE OR REPLACE FUNCTION fn_primeira_letra_disponibilidade()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.disponibilidade :=
+        UPPER(LEFT(LOWER(TRIM(NEW.disponibilidade)), 1)) ||
+        SUBSTRING(LOWER(TRIM(NEW.disponibilidade)) FROM 2);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_primeira_letra_disponibilidade
+BEFORE INSERT OR UPDATE ON Produto
+FOR EACH ROW
+EXECUTE FUNCTION fn_primeira_letra_disponibilidade();
+
+CREATE OR REPLACE PROCEDURE sp_registrar_movimentacao(
+    p_id_produto INTEGER,
+    p_tipo VARCHAR(10),
+    p_motivo VARCHAR(20),
+    p_quantidade INTEGER,
+    p_preco_unitario_praticado NUMERIC(10, 2),
+    p_valor_total NUMERIC(12, 2),
+    p_observacao VARCHAR(255)
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_produto_ativo BOOLEAN;
+BEGIN
+
+    -- Verifica se o produto existe e está ativo.
+    SELECT ativo
+    INTO v_produto_ativo
+    FROM produto
+    WHERE id_produto = p_id_produto;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Produto não encontrado.';
+    END IF;
+
+    IF v_produto_ativo = FALSE THEN
+        RAISE EXCEPTION 'Não é possível movimentar um produto desativado.';
+    END IF;
+
+    -- A quantidade deve ser maior que zero.
+    IF p_quantidade <= 0 THEN
+        RAISE EXCEPTION 'A quantidade deve ser maior que zero.';
+    END IF;
+
+    -- Valida o tipo da movimentação.
+    IF p_tipo NOT IN ('ENTRADA', 'SAIDA') THEN
+        RAISE EXCEPTION 'Tipo de movimentação inválido.';
+    END IF;
+
+    -- Valida o motivo da movimentação.
+    IF p_motivo NOT IN (
+        'RECEBIMENTO',
+        'VENDA',
+        'USO_INTERNO',
+        'PERDA',
+        'DANIFICADO',
+        'CORRECAO'
+    ) THEN
+        RAISE EXCEPTION 'Motivo de movimentação inválido.';
+    END IF;
+
+    -- Venda precisa ser uma saída.
+    IF p_motivo = 'VENDA' AND p_tipo <> 'SAIDA' THEN
+        RAISE EXCEPTION 'Uma venda deve ser uma movimentação de saída.';
+    END IF;
+
+    -- Venda precisa possuir preço e valor total.
+    IF p_motivo = 'VENDA' THEN
+
+        IF p_preco_unitario_praticado IS NULL THEN
+            RAISE EXCEPTION 'Venda deve possuir preço unitário.';
+        END IF;
+
+        IF p_valor_total IS NULL THEN
+            RAISE EXCEPTION 'Venda deve possuir valor total.';
+        END IF;
+
+        IF p_valor_total <> p_quantidade * p_preco_unitario_praticado THEN
+            RAISE EXCEPTION 'O valor total da venda está incorreto.';
+        END IF;
+
+    END IF;
+
+    -- Os demais tipos não devem possuir dados financeiros.
+    IF p_motivo <> 'VENDA' THEN
+
+        IF p_preco_unitario_praticado IS NOT NULL
+           OR p_valor_total IS NOT NULL THEN
+            RAISE EXCEPTION
+                'Somente vendas podem possuir preço unitário e valor total.';
+        END IF;
+
+    END IF;
+
+    -- Observação obrigatória.
+    IF TRIM(p_observacao) = '' THEN
+        RAISE EXCEPTION 'A observação é obrigatória.';
+    END IF;
+
+    -- Insere a movimentação.
+    -- O trigger tg_atualizar_estoque será acionado
+    -- automaticamente e atualizará o estoque do produto.
+    INSERT INTO movimentacao (
+        id_produto,
+        tipo,
+        motivo,
+        quantidade,
+        preco_unitario_praticado,
+        valor_total,
+        observacao
+    )
+    VALUES (
+        p_id_produto,
+        p_tipo,
+        p_motivo,
+        p_quantidade,
+        p_preco_unitario_praticado,
+        p_valor_total,
+        TRIM(p_observacao)
+    );
+
+END;
+$$;
